9ecf260611ebfcb04b1021b32b6cf4c6edeb6baf
MatteoFilosa <filosa.1793997@studenti.uniroma1.it>
2022-03-07 12:19:47 +0100

    s

--- lab_03/server.py
+++ lab_03/server.py
@@ -26,48 +26,6 @@ app.debug = True
 app.secret_key = 'random secret'
 
 
-# oAuth Setup
-oauth = OAuth(app)
-google = oauth.register(
-    name='google',
-    client_id="274085789990-3tmjvlcbdq657p8vf8tsjaloob4q6kei.apps.googleusercontent.com",#os.getenv("GOOGLE_CLIENT_ID"),
-    client_secret="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",
-    access_token_url='https://accounts.google.com/o/oauth2/token',
-    access_token_params=None,
-    authorize_url='https://accounts.google.com/o/oauth2/auth',
-    authorize_params=None,
-    api_base_url='https://www.googleapis.com/oauth2/v1/',
-    userinfo_endpoint='https://openidconnect.googleapis.com/v1/userinfo',  # This is only needed if using openId to fetch user info
-    client_kwargs={'scope': 'openid email profile'},
-)
-
-@app.route('/oauthlogin')
-def login():
-    google = oauth.create_client('google')  # create the google oauth client
-    redirect_uri = url_for('authorize', _external=True)
-    return google.authorize_redirect(redirect_uri)
-
-
-@app.route('/authorize')
-def authorize():
-    google = oauth.create_client('google')  # create the google oauth client
-    token = google.authorize_access_token()  # Access token from google (needed to get user info)
-    resp = google.get('userinfo')  # userinfo contains stuff u specificed in the scrope
-    user_info = resp.json()
-    user = oauth.google.userinfo()  # uses openid endpoint to fetch user info
-    # Here you use the profile/user data that you got and query your database find/register the user
-    # and set ur own data in the session not the profile from google
-    session['profile'] = user_info
-    session.permanent = True  # make the session permanant so it keeps existing after broweser gets closed
-    return redirect('/')
-
-
-@app.route('/oauthlogout')
-def logout():
-    for key in list(session.keys()):
-        session.pop(key)
-    return redirect('/')
-
 #########END OAUTH###########
 #app.config['SECRET_KEY'] = 'secret!'
 #socketio = SocketIO(app)
@@ -108,14 +66,7 @@ def index():
 #@socketio.on('my event')
 #def handle_my_custom_event(json):
     #emit('my response', json)
-@app.route('/forgot', methods=('GET', 'POST'))
-def forgot():
-    error = None
-    message = None
-    form = ForgotForm()
-    if form.validate_on_submit():
-        pass
-    return render_template('static/forgot.html', form=form, error=error, message=message)
+
 
 @app.route('/user/signup', methods = ['POST'])
 def sign_up():
